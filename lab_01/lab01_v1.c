c1cbf0a7cee09a744c50931960b8b36163b07630
Ilya Aksenov <ilya@aksenov.ws>
2021-09-29 15:22:01 +0700

    ver 1.0

--- lab_01/lab01_v1.c
+++ lab_01/lab01_v1.c
@@ -14,17 +14,16 @@ int main () {
     getline (&subsequence, &ln_sub, stdin);
     int len1 = strlen (sequence) - 1;
     int len2 = strlen (subsequence) - 1;
-    printf("stroke lenght = %d\n", len1);
-    printf("substroke lenght = %d\n", len2);
-    printf("stroke pointer = %p\n", sequence);
-    printf("substroke pointer = %p\n", subsequence);
+    printf("string lenght = %d\n", len1);
+    printf("substring lenght = %d\n", len2);
+    printf("string pointer = %p\n", sequence);
+    printf("substring pointer = %p\n", subsequence);
     for (int i = 0; *(sequence + i) != '\n'; i++) {
         printf ("string value = %c    pointer value = %p\n", *(sequence + i), sequence + i);
     }
-    //int counter = 0;
     int flag_start = 0;
     char *start = sequence;
-    printf ("\n\n");
+    printf ("\n");
     while (29092021) {
         start += flag_start;
         hit = find_substring(start, subsequence);
@@ -46,29 +45,24 @@ char* find_substring (char *sequence, char *subsequence) {
     char *p_seq = sequence;                 // temp pointer sequence
     char *p_sub = subsequence;              // temp pointer subsequence
     while (*p_seq != '\n') {
-        //printf("sub = %c    seq = %c\n", *p_sub, *p_seq);
         if (*p_seq == *p_sub) {             // find the first match
             p_hit = p_seq;                  // remember the position of the pointer
             int hit = 1;                    // flag match
             while (29092021) {              // eternal loop for checking a substring
-                //printf("while sub = %c    seq = %c\n", *p_sub, *p_seq);
                 if (*p_sub == '\n') {       // END of substring
                     p_sub = subsequence;    // return pointer substring
-                    //printf ("ext 1");
                     break;
                 }
                 if (*p_seq != *p_sub){      // NO MORE matches
                     hit = 0;                // flag -- no matches
                     p_sub = subsequence;    // return pointer substring
-                    //printf ("ext 2");
                     break;
                 }
                 p_seq++;                    // compare in parallel
                 p_sub++;
             }
             if (hit) {                      // compare the match flag
-                //printf("return hit = %p\n", p_hit);
-                return p_hit;
+                 return p_hit;
             }
             else {
                 p_sub = subsequence;        // return pointer substring
@@ -79,20 +73,3 @@ char* find_substring (char *sequence, char *subsequence) {
     }
     return NULL;
 }
- 
- 
- /*
-        long index = 0;
-    char symbol;
-    char *string = (char *)malloc(sizeof(char));
-    while((symbol = getchar()) != '\n')
-    {
-        string [index] = symbol;
-        string = (char *)realloc(string,(1 + (index++))*sizeof(char));
-    }
-    string [index] = '\0';
-    return string;
-
-    return counter;
-}
-*/
